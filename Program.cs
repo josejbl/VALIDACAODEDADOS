adf716de2113bf82ba928362ee60bfa479229222
FIC <FIC@info.local>
2021-01-08 16:08:28 -0300

    exercicio3

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VALIDACAO_DE_DADOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validação de Dados");
+
+            Console.WriteLine("Digite o nome do usuario");
+            string nome = Console.ReadLine();
+
+            while(nome == ""){
+
+                Console.WriteLine("O nome não popde ser vazio");
+                Console.WriteLine("Digite o nome de usuario");
+                nome = Console.ReadLine();
+
+
+            }
+
+
+        }
+    }
+}
